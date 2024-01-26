166102cfd8a9fc604f01c0d5ad61386140755b8e
dmychel <dmychel@tutanota.com>
2024-01-26 18:33:59 -0500

    created static UI version of component

--- src/components/home/UserInfo.jsx
+++ src/components/home/UserInfo.jsx
@@ -2,29 +2,31 @@ import "/src/styles/userInfo.css";
 
 const UserInfo = () => {
   return (
-    <div className="user-info">
-      <span>
-        OS: <p>Arch Linux</p>
-      </span>
-      <span>
-        Kernal: <p>1.0.0-lts</p>
-      </span>
-      <span>
-        Uptime: <p>1 hour, 5 mins</p>
-      </span>
-      <span>
-        Packages: <p>100 (npm)</p>
-      </span>
-      <span>
-        Resolutuin: <p>1920x1080</p>
-      </span>
-      <span>
-        Theme: <p>Qogir-manjaro-dark</p>
-      </span>
-      <span>
-        Terminal: <p>Font</p>
-      </span>
-    </div>
+    <section className="user-info-container">
+      <div className="user-info">
+        <span>
+          OS: <p>Arch Linux</p>
+        </span>
+        <span>
+          Kernal: <p>1.0.0-lts</p>
+        </span>
+        <span>
+          Uptime: <p>1 hour, 5 mins</p>
+        </span>
+        <span>
+          Packages: <p>100 (npm)</p>
+        </span>
+        <span>
+          Resolutuin: <p>1920x1080</p>
+        </span>
+        <span>
+          Theme: <p>Qogir-manjaro-dark</p>
+        </span>
+        <span>
+          Terminal: <p>Font</p>
+        </span>
+      </div>
+    </section>
   );
 };
 
