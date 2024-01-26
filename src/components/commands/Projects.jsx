a36fdf02160e5f1d0ddfa5e59753d2234366d14d
dmychel <dmychel@tutanota.com>
2024-01-26 18:33:40 -0500

    created static UI version of component

---
+++ src/components/commands/Projects.jsx
@@ -0,0 +1,19 @@
+import "/src/styles/projects.css";
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <div className="tip">
+        <span>Tip:</span> Try typing
+        <span style={{ marginLeft: "1%" }}>
+          &apos;cd windfall-matchup&apos;
+        </span>
+      </div>
+      <span>Windfall Matchup</span>
+      <span>Video Game Archive</span>
+      <span>CV Maker</span>
+    </section>
+  );
+};
+
+export default Projects;
