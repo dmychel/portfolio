d0ff327acf372851f8e1561cf997d8200f1c5074
dmychel <dmychel@tutanota.com>
2024-01-26 19:35:10 -0500

    created useState for command input and function for handling submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import CommandLine from "./components/CommandLine";
 import Home from "/src/components/home/Home";
 
 function App() {
+  const [command, setCommand] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
     <section className="app">
       <Home />
-      <CommandLine />
+      <CommandLine
+        handleSubmit={handleSubmit}
+        command={command}
+        setCommand={setCommand}
+      />
     </section>
   );
 }
