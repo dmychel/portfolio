32045b340525933249d68b2577e9484d928d63da
dmychel <dmychel@tutanota.com>
2024-01-28 19:20:46 -0500

    ascii component dynamically renders ascii art component based on users OS

--- src/components/home/ascii/Ascii.jsx
+++ src/components/home/ascii/Ascii.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import '/src/styles/ascii.css'
+
+import Windows from "./Windows";
+import Linux from "./Linux";
+
 const Ascii = () => {
-  return;
+  const [os, setOs] = useState([]);
+
+  useEffect(() => {
+    if (navigator.appVersion.indexOf("Wind") != -1) setOs([Windows]);
+    if (navigator.appVersion.indexOf("Mac") != -1) setOs("Mac");
+    if (navigator.appVersion.indexOf("Linux") != -1) setOs("Linux");
+  }, []);
+
+
+  return (
+    <section className="ascii-wrapper">
+      {os.map((comp) => {
+        const CompName = comp;
+        return <CompName key={crypto.randomUUID()} />;
+      })}
+    </section>
+  );
 };
+
+export default Ascii;
