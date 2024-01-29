71e60da55a38e60b34760cca62fd2ccf36bcb3bc
dmychel <dmychel@tutanota.com>
2024-01-28 23:09:49 -0500

    ascii jsx

--- src/components/home/ascii/Windows.jsx
+++ src/components/home/ascii/Windows.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 import '/src/styles/ascii.css'
 
 const Windows = () => {
   return (
     <div className="ascii-art">
-
-        <span>                            .oodMMMM</span>
+      <span>                            .oodMMMM</span>
         <span>                  .oodMMMMMMMMMMMMM</span>
         <span>      ..oodMMM   MMMMMMMMMMMMMMMMMMM</span>
         <span>oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
@@ -13,7 +13,7 @@ const Windows = () => {
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
-        <span>                                      </span>
+        <span>                                   </span>
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
@@ -21,10 +21,31 @@ const Windows = () => {
         <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>`^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
         <span>      ````^^^^   ^^MMMMMMMMMMMMMMMMM</span>
-        <span>                       ````^^^^^^MMMM</span>          
+        <span>                       ````^^^^^^MMMM</span>    
+    
+    
     </div>
   )
 };
 
 export default Windows;
 
+{/*     
+      <span>           ,::=:!!t3Z3z.                </span>
+      <span>          Et:::ztt33EEEL  @Ee,.     .., </span>
+      <span>         ;tt:::tt333EE7 ;EEEEEEttttt33# </span>   
+      <span>         :Et:::zt333EEQ. $EEEEEttttt33QL</span>
+      <span>        it::::tt333EEF  @EEEEEEttrtt33F </span>
+      <span>       ;3=*^```"*4EEV  :EEEEEttttt33@.  </span>
+      <span>      ,.=::::!t=., :   @EEEEEtttz33QF   </span>    
+      <span>     ;::::::::zt33      "4EEEtttji3P*   </span> 
+      <span>    :t::::::::tt33.:Z3z..    ``,..g.    </span>
+      <span>   i::::::::zt33F  AEEEtttt::::ztF      </span>
+      <span>  ;::::::::::t33v  ;EEEttttt::::t3      </span>
+      <span>  E::::::::::zt   @EEEtttt""""z3F       </span>
+      <span> 3=*^```"*4E3)   ;EEEtttt:::::tZ        </span>
+      <span>                 :EEEEtttt::::z7        </span>
+      <span>                 `-:"VEzjt:;;^*`        </span>   */}
+
+      
+        
