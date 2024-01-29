400fbe40847786968798533bdcfc2a545028e749
dmychel <dmychel@tutanota.com>
2024-01-28 23:10:31 -0500

    set parameters for useeffect

--- src/components/home/ascii/Ascii.jsx
+++ src/components/home/ascii/Ascii.jsx
@@ -3,14 +3,16 @@ import '/src/styles/ascii.css'
 
 import Windows from "./Windows";
 import Linux from "./Linux";
+import Mac from "./Mac";
 
 const Ascii = () => {
   const [os, setOs] = useState([]);
 
   useEffect(() => {
+    console.log(navigator.appVersion)
     if (navigator.appVersion.indexOf("Wind") != -1) setOs([Windows]);
-    if (navigator.appVersion.indexOf("Mac") != -1) setOs("Mac");
-    if (navigator.appVersion.indexOf("Linux") != -1) setOs("Linux");
+    if (navigator.appVersion.indexOf("Mac") != -1) setOs([Mac]);
+    if (navigator.appVersion.indexOf("X11" || 'Linux') != -1) setOs([Linux]);
   }, []);
 
 
