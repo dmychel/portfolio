99e06b19b4d8636bd4c43d74246e6cf284f0169f
dmychel <dmychel@tutanota.com>
2024-01-28 19:21:18 -0500

    windows ascii art crreated

---
+++ src/components/home/ascii/Windows.jsx
@@ -0,0 +1,30 @@
+import '/src/styles/ascii.css'
+
+const Windows = () => {
+  return (
+    <div className="ascii-art">
+
+        <span>                            .oodMMMM</span>
+        <span>                  .oodMMMMMMMMMMMMM</span>
+        <span>      ..oodMMM   MMMMMMMMMMMMMMMMMMM</span>
+        <span>oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>                                      </span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>`^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM</span>
+        <span>      ````^^^^   ^^MMMMMMMMMMMMMMMMM</span>
+        <span>                       ````^^^^^^MMMM</span>          
+    </div>
+  )
+};
+
+export default Windows;
+
