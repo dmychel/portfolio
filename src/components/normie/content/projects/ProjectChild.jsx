3f56c1630607f0c3f6f35776f410e0d51a38bf8b
dmychel <dmychel@tutanota.com>
2024-01-31 16:39:09 -0500

    removed all functions as they are not needed

--- src/components/normie/content/projects/ProjectChild.jsx
+++ src/components/normie/content/projects/ProjectChild.jsx
@@ -2,43 +2,11 @@ import PropTypes from "prop-types";
 import "/src/styles/normie/projects.css";
 
 const ProjectChild = ({ project }) => {
-  const MouseOver = (id, id2) => {
-    console.log(id2);
-    const prevEl = document.getElementById(id);
-    const imageEl = document.getElementById(id2);
-
-    prevEl.style.left = 0;
-    imageEl.style.opacity = 0.75;
-  };
-
-  const mouseExit = (id, id2) => {
-    console.log(id2);
-    const prevEl = document.getElementById(id);
-    const imageEl = document.getElementById(id2);
-
-    prevEl.style.left = "-5000px";
-    imageEl.style.opacity = 0;
-  };
 
   return (
     <>
-      <div className="preview-img" id="preview">
-        <img id={project.name} src={project.image} alt="image" />
-      </div>
-
-      <div
-        className="project"
-        onMouseMove={() =>
-          MouseOver(project.name, project.name + " background")
-        }
-        onMouseLeave={() =>
-          mouseExit(project.name, project.name + " background")
-        }
-      >
-        <div className="project-background" id={project.name + " background"}>
-          <img src={project.background} alt="" />
-        </div>
-        <p>{project.name}</p>
+      <div className="project">
+        <p className="project-title">{project.name}</p>
         <p className="description">{project.description}</p>
         <nav className="links">
           <a href={project.link} target="_blank" rel="noreferrer">
