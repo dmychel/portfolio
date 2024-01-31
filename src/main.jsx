b0b7a8a74a005df5ab03d4c0c74de9be26766fef
dmychel <dmychel@tutanota.com>
2024-01-31 15:38:44 -0500

    added routing path

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import NormieView from "./NormieView";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: 'normie',
+    element: <NormieView/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
