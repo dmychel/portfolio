0962c7e513b4080709541e45b67650a7861d0662
dmychel <dmychel@tutanota.com>
2024-01-31 13:26:32 -0500

    added conditions for other os's

--- src/components/directories/home/ascii/Ascii.jsx
+++ src/components/directories/home/ascii/Ascii.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 import "/src/styles/ascii.css";
 import Windows from "./Windows";
+import Mac from "./Mac";
+import Linux from "./Linux";
 
 const Ascii = ({ os }) => {
   const dynamicComp = () => {
     if (os === "windows") return <Windows />;
+    if (os === "mac") return <Mac />;
+    if (os === "linux") return <Linux />;
     console.log("run");
   };
   return <section className="ascii-wrapper">{dynamicComp()}</section>;
