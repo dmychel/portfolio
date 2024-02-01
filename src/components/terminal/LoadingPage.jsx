03a553f9730b3f1aa2e2bfc9564bb7798729afc8
dmychel <dmychel@tutanota.com>
2024-02-01 17:49:15 -0500

    added type animation

--- src/components/terminal/LoadingPage.jsx
+++ src/components/terminal/LoadingPage.jsx
@@ -1,5 +1,85 @@
-const Loading = () => {
-  return <section className="loading">Hello World, were loading</section>;
+import { TypeAnimation } from "react-type-animation";
+import PropTypes from "prop-types";
+import "/src/styles/terminal/loading.css";
+
+const Loading = ({ os, setLoading }) => {
+  setTimeout(() => {
+    setLoading(false);
+  }, 12000);
+
+  const determineMessage = () => {
+    if (os === "Linux") return linuxMessage();
+    if (os === "Windows") return windowsMessage();
+    if (os === "Mac") return appleMessage();
+  };
+
+  const windowsMessage = () => {
+    return (
+      <TypeAnimation
+        sequence={[
+          "detecting operating system...",
+          1000,
+          "oh no..",
+          1000,
+          `you're using windows..`,
+          1000,
+          `at least it's not apple ;)`,
+          1000,
+          "migrating you to the terminal now",
+        ]}
+        speed={75}
+      />
+    );
+  };
+
+  const appleMessage = () => {
+    return (
+      <TypeAnimation
+        sequence={[
+          "detecting operating system...",
+          1000,
+          "oh..",
+          1000,
+          `you're using apple..`,
+          500,
+          `you're using apple...`,
+          500,
+          `you're using apple....`,
+          1000,
+          "migrating you to the terminal now",
+        ]}
+        speed={75}
+      />
+    );
+  };
+
+  const linuxMessage = () => {
+    return (
+      <TypeAnimation
+        sequence={[
+          "detecting operating system...",
+          1000,
+          "oh.. I see.",
+          500,
+          `you're using Linux..`,
+          800,
+          `Linux the master race :)`,
+          500,
+          `Welcome friend`,
+          1000,
+          "migrating you to the terminal now",
+        ]}
+        speed={80}
+      />
+    );
+  };
+
+  return <section className="loading">{determineMessage()}</section>;
+};
+
+Loading.propTypes = {
+  os: PropTypes.string,
+  setLoading: PropTypes.func,
 };
 
 export default Loading;
