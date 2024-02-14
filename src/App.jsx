0b53f04cc6257c4c33b3c3e7d333c6a984ae75c3
dmychel <dmychel@tutanota.com>
2024-02-13 22:21:25 -0500

    jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <section className="app">
       <div className="title">
-        <p>Select your path</p>
+        <p>Select your path:</p>
       </div>
       <div className="options">
         <Link to="normie">Normie View</Link>
