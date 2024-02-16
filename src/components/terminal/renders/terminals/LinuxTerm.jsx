e6092e9fa221770ca952419b210374bcf3f65ebe
dmychel <dmychel@tutanota.com>
2024-02-15 20:17:56 -0500

    added useEffect function that scrolls to bottom of div and added classnames

--- src/components/terminal/renders/terminals/LinuxTerm.jsx
+++ src/components/terminal/renders/terminals/LinuxTerm.jsx
@@ -1,12 +1,33 @@
 import RenderContent from "../RenderContent";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 
 import styles from "/src/styles/terminal/_LinuxTerm.module.scss";
 
-const LinuxTerm = ({ renderComp, os }) => {
+const LinuxTerm = ({
+  renderComp,
+  os,
+  handleSubmit,
+  handleKeyDown,
+  command,
+  setCommand,
+}) => {
+  useEffect(() => {
+    console.log("window scroll run");
+    const termEL = document.getElementById("terminal");
+    termEL.scrollTo(0, termEL.scrollHeight);
+  }, [setCommand]);
+
   return (
-    <div className={styles.terminal}>
-      <RenderContent renderComp={renderComp} os={os} />
+    <div className={styles.terminal} id="terminal">
+      <RenderContent
+        renderComp={renderComp}
+        os={os}
+        handleSubmit={handleSubmit}
+        handleKeyDown={handleKeyDown}
+        command={command}
+        setCommand={setCommand}
+      />
     </div>
   );
 };
@@ -14,6 +35,10 @@ const LinuxTerm = ({ renderComp, os }) => {
 LinuxTerm.propTypes = {
   renderComp: PropTypes.array,
   os: PropTypes.string,
+  handleSubmit: PropTypes.func,
+  handleKeyDown: PropTypes.func,
+  command: PropTypes.string,
+  setCommand: PropTypes.func,
 };
 
 export default LinuxTerm;
