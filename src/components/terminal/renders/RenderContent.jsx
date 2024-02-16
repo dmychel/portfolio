9db3dfb94ac0dba4390187a69cee82b30f06b2bd
dmychel <dmychel@tutanota.com>
2024-02-15 20:18:18 -0500

    added props

--- src/components/terminal/renders/RenderContent.jsx
+++ src/components/terminal/renders/RenderContent.jsx
@@ -1,21 +1,39 @@
 import PropTypes from "prop-types";
 import CommandLine from "/src/components/terminal/CommandLine";
 
-const RenderContent = ({ renderComp }) => {
-  console.log(renderComp);
+import styles from "/src/styles/terminal/terminal.module.scss";
+
+const RenderContent = ({
+  renderComp,
+  os,
+  handleSubmit,
+  handleKeyDown,
+  command,
+  setCommand,
+}) => {
   return (
-    <>
+    <div className={styles.content}>
       {renderComp.map((comp) => {
         const CompName = comp;
-        return <CompName key={crypto.randomUUID()} />;
+        return <CompName key={crypto.randomUUID()} os={os} />;
       })}
-      <CommandLine />
-    </>
+      <CommandLine
+        handleSubmit={handleSubmit}
+        handleKeyDown={handleKeyDown}
+        command={command}
+        setCommand={setCommand}
+      />
+    </div>
   );
 };
 
 RenderContent.propTypes = {
   renderComp: PropTypes.array,
+  os: PropTypes.string,
+  handleSubmit: PropTypes.func,
+  handleKeyDown: PropTypes.func,
+  command: PropTypes.string,
+  setCommand: PropTypes.func,
 };
 
 export default RenderContent;
