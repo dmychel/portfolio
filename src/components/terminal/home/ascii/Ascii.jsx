ed8ec121dc6b9d2f3cbf50386b122216a1a4c100
dmychel <dmychel@tutanota.com>
2024-03-01 11:15:52 -0500

    added condition for default render

--- src/components/terminal/home/ascii/Ascii.jsx
+++ src/components/terminal/home/ascii/Ascii.jsx
@@ -2,15 +2,19 @@ import PropTypes from "prop-types";
 import Windows from "./Windows";
 import Mac from "./Mac";
 import Linux from "./Linux";
+import Default from "./Default";
 
 import styles from "/src/styles/terminal/ascii.module.scss";
+
 
 const Ascii = ({ os }) => {
   const dynamicComp = () => {
     if (os === "Windows") return <Windows />;
     if (os === "Mac") return <Mac />;
     if (os === "Linux") return <Linux />;
-    console.log("run");
+    else {
+      return <Default/>
+    }
   };
   return <section className={styles.asciiWrapper}>{dynamicComp()}</section>;
 };
