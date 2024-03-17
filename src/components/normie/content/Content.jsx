69df6109eca9d5a5b1bdb065a51838ceeee22db2
dmychel <dmychel@tutanota.com>
2024-03-17 16:40:55 -0400

    changed jsx

--- src/components/normie/content/Content.jsx
+++ src/components/normie/content/Content.jsx
@@ -1,18 +1,16 @@
 import About from "/src/components/normie/content/About";
 import Projects from "/src/components/normie/content/projects/Projects";
 import Contact from "/src/components/normie/content/Contact";
-import Modal from "../../Modal";
 
 import styles from "/src/styles/normie.module.scss";
 
 const Content = () => {
   return (
-    <section className={styles.contentMaster}>
-      <Modal />
+    <div className={styles.contentMaster}>
       <About />
       <Projects />
       <Contact />
-    </section>
+    </div>
   );
 };
 
