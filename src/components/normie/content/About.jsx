979508fa0c23603b8918f7ce2f0543315da60bb4
dmychel <dmychel@tutanota.com>
2024-03-21 16:51:06 -0400

    carousel logic created with the use of useState and class names

--- src/components/normie/content/About.jsx
+++ src/components/normie/content/About.jsx
@@ -1,7 +1,46 @@
 import styles from "/src/styles/about.module.scss";
 import Header from "/src/components/normie/Header";
+import { useState, useEffect } from "react";
 
 const About = () => {
+  const imageArr = ["hero", "break", "coffee"];
+  const [slider, setSlider] = useState("hero");
+
+  useEffect(() => {
+    const element = document.getElementById(slider);
+    element.style.display = "block";
+  }, [slider]);
+
+  function nextImg() {
+    const index = imageArr.indexOf(slider);
+    if (index === 2) {
+      const newEl = imageArr[0];
+      const prevImg = document.getElementById(slider);
+      prevImg.style.display = "none";
+      setSlider(newEl);
+    } else {
+      const newEl = imageArr[index + 1];
+      const prevImg = document.getElementById(slider);
+      prevImg.style.display = "none";
+      setSlider(newEl);
+    }
+  }
+
+  function previousImg() {
+    const index = imageArr.indexOf(slider);
+    if (index === 0) {
+      const newEl = imageArr[2];
+      const prevImg = document.getElementById(slider);
+      prevImg.style.display = "none";
+      setSlider(newEl);
+    } else {
+      const newEl = imageArr[index - 1];
+      const prevImg = document.getElementById(slider);
+      prevImg.style.display = "none";
+      setSlider(newEl);
+    }
+  }
+
   return (
     <section className={styles.about_container}>
       <Header />
@@ -10,15 +49,23 @@ const About = () => {
           <div className={styles.images}>
             <img
               src="/assets/imgs/slider/hero.JPG"
-              alt="hero"
-              className={styles.active}
+              alt="hero photo"
+              id="hero"
             />
-            <img src="/assets/imgs/slider/break.jpg" alt="hero" />
-            <img src="/assets/imgs/slider/coffee.jpg" alt="hero" />
+            <img
+              src="/assets/imgs/slider/break.jpg"
+              alt="break photo"
+              id="break"
+            />
+            <img
+              src="/assets/imgs/slider/coffee.jpg"
+              alt="coffee photo"
+              id="coffee"
+            />
           </div>
           <div className={styles.nav}>
-            <button>&lt;</button>
-            <button>&gt;</button>
+            <button onClick={previousImg}>&lt;</button>
+            <button onClick={nextImg}>&gt;</button>
           </div>
         </div>
         <div className={styles.info}>
