0622deaae1150794cec9b45355e575f81f11b00c
dmychel <dmychel@tutanota.com>
2024-03-21 15:18:40 -0400

    added about route path

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import About from "./components/normie/content/About";
 import Terminal from "./Terminal";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   },
   {
     path: "terminal",
