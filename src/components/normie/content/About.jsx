e0fd6af15689c83b54bf92e2f432275e727737b7
dmychel <dmychel@tutanota.com>
2024-03-21 16:25:19 -0400

    changed jsx structure

--- src/components/normie/content/About.jsx
+++ src/components/normie/content/About.jsx
@@ -1,17 +1,35 @@
-import styles from "/src/styles/normie.module.scss";
+import styles from "/src/styles/about.module.scss";
+import Header from "/src/components/normie/Header";
 
 const About = () => {
   return (
-    <section className={styles.aboutMaster}>
+    <section className={styles.about_container}>
+      <Header />
       <div className={styles.about}>
-        <div className={styles.leftAbout}>
-          <h1>Whats up! My name is Dylon</h1>
-          <p>
-            A self taught developer with a burning passion to learn and create.
-          </p>
+        <div className={styles.slider}>
+          <div className={styles.images}>
+            <img
+              src="/assets/imgs/slider/hero.JPG"
+              alt="hero"
+              className={styles.active}
+            />
+            <img src="/assets/imgs/slider/break.jpg" alt="hero" />
+            <img src="/assets/imgs/slider/coffee.jpg" alt="hero" />
+          </div>
+          <div className={styles.nav}>
+            <button>&lt;</button>
+            <button>&gt;</button>
+          </div>
         </div>
-        <div className={styles.rightAbout}>
-          <img src="/assets/imgs/hero.JPG" alt="hero image" />
+        <div className={styles.info}>
+          <h3>Hello!</h3>
+          <h2>My name is Dylon</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,
+            quibusdam eos unde ex sint asperiores quod soluta culpa tempora
+            atque dolorem nulla! Iure modi exercitationem labore. Reiciendis
+            ratione consequuntur eveniet!
+          </p>
         </div>
       </div>
     </section>
