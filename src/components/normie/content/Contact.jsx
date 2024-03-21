987e170a363524d8a4f89d1ab408f55f4881b742
dmychel <dmychel@tutanota.com>
2024-03-21 14:23:32 -0400

    jsx structure changes

--- src/components/normie/content/Contact.jsx
+++ src/components/normie/content/Contact.jsx
@@ -2,42 +2,40 @@ import styles from "/src/styles/normie.module.scss";
 
 const Contact = () => {
   return (
-    <section className={styles.contactMaster}>
-      <div className={styles.title}>
-        <h2>Contact</h2>
+    <section className={styles.contact_container}>
+      <h2>Say Hello</h2>
+
+      <div className={styles.contact_info}>
+        <p>
+          If you&apos;re looking to get in touch there are a few ways you can
+          contact me:
+          <br />
+          <br />
+          Email me at{" "}
+          <a href="mailto:dyloncrowley86@gmail.com"> Hey@DylonCrowley.tech</a>
+          <br />
+          Message me on <a href="">LinkedIn</a>
+        </p>
       </div>
-      <div className={styles.contact}>
-        <div className={styles.contactText}>
-          <p>
-            Here are some of the ways you can reach out. I am most likely to see
-            and respond to any messages sent to my email,
-            <br />
-            <span>Hey@DylonCrowley.tech</span>
-          </p>
-        </div>
-        <div className={styles.contactIMG}>
-          <a href="mailto:hey@dyloncrowley.tech">
-            <img
-              src="/assets/icons/mail.png"
-              alt="Email"
-              target="_blank"
-              rel="noreferrer"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/dylon-crowley-perez-248b0823b/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="/assets/icons/linkedin.png" alt="LinkedIN" />
-          </a>
-          <a href="https://github.com/dmychel" target="_blank" rel="noreferrer">
-            <img src="/assets/icons/github.png" alt="Github" />
-          </a>
-          <a href="/assets/imgs/resume.pdf" target="_blank" rel="noreferrer">
-            <img src="/assets/icons/resume.png" alt="Resume" />
-          </a>
-        </div>
+
+      <div className={styles.contact_imgs}>
+        <a href="https://github.com/dmychel" target="_blank" rel="noreferrer">
+          <img src="/assets/icons/github.png" alt="github" />
+        </a>
+        <a
+          href="mailto:dyloncrowley86@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/assets/icons/mail.png" alt="email" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dylon-crowley-perez-248b0823b/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/assets/icons/linkedin.png" alt="linkedin" />
+        </a>
       </div>
     </section>
   );
