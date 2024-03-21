481936dd6b822620f0872bc90278e59ddf0e8c41
dmychel <dmychel@tutanota.com>
2024-03-21 15:14:04 -0400

    jsx update to make menu functional with the use of useState

--- src/components/normie/Header.jsx
+++ src/components/normie/Header.jsx
@@ -1,7 +1,14 @@
 import PropTypes from "prop-types";
+import styles from "/src/styles/normie.module.scss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = ({ setComponent }) => {
+  const [toggle, setToggle] = useState(false);
+
+  function toggleMenu() {
+    setToggle(!toggle);
+  }
   return (
     <header>
       <h1 onClick={() => setComponent(null)}>
@@ -9,9 +16,23 @@ const Header = ({ setComponent }) => {
       </h1>
 
       <nav>
-        <span></span>
-        <span></span>
-        <span></span>
+        {toggle ? (
+          <div className={styles.nav_page}>
+            <div className={styles.nav_menu_toggled} onClick={toggleMenu}>
+              <span>&#10006;</span>
+            </div>
+            <div className={styles.nav_options}>
+              <h2>Portfolio</h2>
+              <h2>About</h2>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.nav_menu} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        )}
       </nav>
     </header>
   );
