557ec215312bf3e6471c545c0b0edf87909514f5
dmychel <dmychel@tutanota.com>
2024-03-21 17:57:44 -0400

    added div for project info

--- src/components/normie/content/projects/ProjectChild.jsx
+++ src/components/normie/content/projects/ProjectChild.jsx
@@ -7,6 +7,14 @@ const ProjectChild = ({ project }) => {
       <div className={styles.background}>
         <img src={project.background} />
       </div>
+      <div className={styles.project_info}>
+        <h2>{project.name}</h2>
+        <div className={styles.tools}>
+          {project.tools.map((tool) => (
+            <img src={tool.img} alt={tool.name} key={tool.name}></img>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
