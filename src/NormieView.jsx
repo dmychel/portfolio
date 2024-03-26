de7b0b4286a4ef15725a17c66f8c4c24f4f1246a
dmychel <dmychel@tutanota.com>
2024-03-25 20:23:22 -0400

    created useffect statment that changes loading state tp false

--- src/NormieView.jsx
+++ src/NormieView.jsx
@@ -2,14 +2,29 @@ import styles from "/src/styles/normie.module.scss";
 import Header from "./components/normie/Header";
 import Projects from "./components/normie/content/projects/Projects";
 import Contact from "./components/normie/content/Contact";
+import Loading from './components/Loading';
+import { useEffect, useState } from "react";
 
 const NormieView = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500)
+  }, [])
   return (
-    <div className={styles.normie_app}>
-      <Header />
-      <Projects />
-      <Contact />
-    </div>
+    <>
+      {
+        loading ? <Loading /> :
+          <div className={styles.normie_app}>
+            <Header />
+            <Projects />
+            <Contact />
+          </div>
+      }
+    </>
+
   );
 };
 
