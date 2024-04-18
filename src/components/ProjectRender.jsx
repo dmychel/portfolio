18878079cf8ceb4b2718e71dd173a2d6d7d1f8e5
dmychel <dmychel@tutanota.com>
2024-04-17 22:41:58 -0400

    removed import

--- src/components/ProjectRender.jsx
+++ src/components/ProjectRender.jsx
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 function ProjectRender({ projects }) {
   return projects.map((object) => (
     <div className="project" key={object.title}>
-      <h2>{object.title}</h2>
-      <p>{object.desctiption}</p>
-      <div className="links">
-        <a href={object.live} target="_blank" rel="noreferrer">
-          View Now
-        </a>
-        <a href={object.repo} target="_blank" rel="noreferrer">
-          Code
-        </a>
+      <div className="left">
+
+        <div className="tools">
+          {object.tools.map((tool) => (
+            <img src={tool.url} alt={tool.name} key={tool.name} />
+          ))}
+          <p>{object.live}</p>
+        </div>
+        <div className="info">
+          <p>{object.description}</p>
+        </div>
+
+      </div>
+      <div className="right">
+        <a href={object.live} target="_blank" rel="noreferrer">&#128279;</a>
       </div>
     </div>
   ));
