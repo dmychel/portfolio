a6c81371a0fb8b3a5e80bb3303bbd011fd45ef38
dmychel <dmychel@tutanota.com>
2024-04-17 20:08:31 -0400

    set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./routes/Index";
+import Projects from "./routes/Projects";
+import About from "./routes/About";
+import Labs from "./routes/Labs";
+import Error from "./routes/Error";
+
+import "/src/styles/app.css";
+
 const App = () => {
-  return;
+  return (
+    <section className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/labs" element={<Labs />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  );
 };
 
 export default App;
