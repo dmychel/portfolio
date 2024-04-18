5bf883097ecc329c439bdf296aadf339abcb9552
dmychel <dmychel@tutanota.com>
2024-04-18 01:06:32 -0400

    jsx updates

--- src/components/ProjectRender.jsx
+++ src/components/ProjectRender.jsx
@@ -1,36 +1,25 @@
 import PropTypes from "prop-types";
-import { motion } from "framer-motion"
 
 function ProjectRender({ projects }) {
   return projects.map((object) => (
-    <motion.div className="project" key={object.title}
-      initial={{
-        opacity: 0,
-        y: "200vh"
-      }}
-      animate={{
-        opacity: 1,
-        y: 0
-      }}
-      transition={{ delay: 1.25, type: "spring", stiffness: 50 }}>
+    <div className="project" key={object.title}>
       <div className="left">
-
         <div className="tools">
           <p>{object.title}</p>
           {object.tools.map((tool, index) => (
             <img src={tool.url} alt={tool.name} key={index} />
           ))}
-
         </div>
         <div className="info">
           <p>{object.description}</p>
         </div>
-
       </div>
       <div className="right">
-        <a href={object.live} target="_blank" rel="noreferrer">&#128279;</a>
+        <a href={object.live} target="_blank" rel="noreferrer">
+          &#128279;
+        </a>
       </div>
-    </motion.div>
+    </div>
   ));
 }
 
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 import ProjectRender from "../components/ProjectRender";
 
 function Projects() {
@@ -9,11 +10,17 @@ function Projects() {
         "A restful API I created to serve media assets from the Yakzua series",
       live: "https://yakuza-api.netlify.app/",
       image: "/assets/imgs/yakuza-api.png",
-      tools: [{ name: "Javascript", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" }, {
-        name: "node", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
-
-      }],
-      id: 1
+      tools: [
+        {
+          name: "Javascript",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+        },
+        {
+          name: "node",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+        },
+      ],
+      id: 1,
     },
     {
       title: "Terminal",
@@ -23,34 +30,39 @@ function Projects() {
       image: "/assets/imgs/yakuza-api.png",
       tools: [
         {
-          name: "reactJS", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+          name: "reactJS",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
         },
         {
-          name: "Vite", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
         },
         {
-          name: "Sass", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
+          name: "Sass",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
         },
       ],
-      id: 2
+      id: 2,
     },
     {
       title: "Onitsuka",
-      description:
-        "A mock eCommerce website",
+      description: "A mock eCommerce website",
       live: "https://onitsuka-tiger-mock.netlify.app/",
       tools: [
         {
-          name: "reactJS", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+          name: "reactJS",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
         },
         {
-          name: "Vite", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
         },
         {
-          name: "Sass", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
+          name: "Sass",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
         },
       ],
-      id: 3
+      id: 3,
     },
     {
       title: "Message Board",
@@ -59,105 +71,114 @@ function Projects() {
       live: "https://message-board-apba.onrender.com/",
       tools: [
         {
-          name: "Node", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+          name: "Node",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
         },
         {
-          name: "MongoDB", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg"
+          name: "MongoDB",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
         },
         {
-          name: "Mongoose", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg"
+          name: "Mongoose",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg",
         },
       ],
-      id: 4
+      id: 4,
     },
     {
       title: "Windfall Matchup",
-      description:
-        "Traditional Match up game to display my competency React",
+      description: "Traditional Match up game to display my competency React",
       live: "https://windfall-matchup.netlify.app/",
       tools: [
         {
-          name: "reactJS", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+          name: "reactJS",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
         },
         {
-          name: "Vite", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
         },
         {
-          name: "Vite", url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
         },
       ],
-      id: 5
+      id: 5,
     },
   ];
 
   return (
     <section className="wrapper">
-      <motion.div
-        className="projects"
-        initial={{
-          borderRadius: "35px",
-          scale: 0.5,
-          opacity: 0.25,
-          rotateZ: 10,
-        }}
-        animate={{
-          scale: 1,
-          height: "95%",
-          width: "95%",
-          opacity: 1,
-          rotateZ: 360,
-        }}
-        transition={{ type: "spring", stiffness: 35 }}
-      >
-        <div className="skills">
-          <motion.h2
-            initial={{
-              opacity: 0,
-              y: "200vh"
-            }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-            transition={{ delay: 1, type: "spring", stiffness: 50 }}>Skills</motion.h2>
-          <motion.div className="skill_tools"
-            initial={{
-              opacity: 0,
-              y: "200vh"
-            }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-            transition={{ delay: 1, type: "spring", stiffness: 50 }}>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="Javascript" />
+      <div className="projects">
+        <motion.span
+          className="back"
+          initial={{ x: "-150vw", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 40 }}
+        >
+          <Link to="/">Back</Link>
+        </motion.span>
+        <motion.div
+          className="skills"
+          initial={{
+            opacity: 0,
+            y: "200vh",
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{ delay: 0.25, type: "spring", stiffness: 50 }}
+        >
+          <h2>Skills</h2>
+          <div className="skill_tools">
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+              alt="Javascript"
+            />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="React" />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+              alt="React"
+            />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" alt="Node" />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="Node"
+            />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg" alt="Express" />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg"
+              alt="Express"
+            />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB" />
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="MongoDB"
+            />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" alt="Sass" />
-          </motion.div>
-        </div>
-        <div className="projects_wrapper">
-          <motion.h2
-            initial={{
-              opacity: 0,
-              y: "200vh"
-            }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-            transition={{ delay: 1.25, type: "spring", stiffness: 50 }}>Projects</motion.h2>
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
+              alt="Sass"
+            />
+          </div>
+        </motion.div>
+        <motion.div
+          className="projects_wrapper"
+          initial={{
+            opacity: 0,
+            y: "200vh",
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
+        >
+          <h2>Projects</h2>
           <ProjectRender projects={projectArr} />
-        </div>
-
-      </motion.div>
+        </motion.div>
+      </div>
     </section>
   );
 }
