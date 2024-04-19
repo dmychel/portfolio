7a866d5541f0c4a5f66265e42e75ac273b062845
dmychel <dmychel@tutanota.com>
2024-04-19 19:46:04 -0400

    jsx edit

--- src/components/ProjectRender.jsx
+++ src/components/ProjectRender.jsx
@@ -1,29 +1,38 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 function ProjectRender({ projects }) {
   return projects.map((object) => (
-
-    <motion.div className="project" key={object.title} whileHover={{ x: 50, scale: 1.05, }} >
-      <Link to={object.page}> <div className="tools">
-        <h4>{object.title}</h4>
-        {object.tools.map((tool, index) => (
-          <motion.img src={tool.url} alt={tool.name} key={index} initial={{
-            y: 20,
-            opacity: 0
-          }}
-            animate={{
-              y: 0,
-              opacity: 1
-            }}
-            transition={{ delay: 1.25 }} />
-        ))}
-      </div>
+    <motion.div
+      className="project"
+      key={object.title}
+      whileHover={{ x: 50, scale: 1.05 }}
+    >
+      <Link to="/project-page">
+        <div className="tools">
+          <h4>{object.title}</h4>
+          {object.tools.map((tool, index) => (
+            <motion.img
+              src={tool.url}
+              alt={tool.name}
+              key={index}
+              initial={{
+                y: 20,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{ delay: 1.25 }}
+            />
+          ))}
+        </div>
         <div className="info">
           <p>{object.description}</p>
-        </div></Link>
-
+        </div>
+      </Link>
     </motion.div>
   ));
 }
