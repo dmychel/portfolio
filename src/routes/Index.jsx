45ffbd7f195f0289e0e0709e1c5757880cb0e284
dmychel <dmychel@tutanota.com>
2024-04-18 20:04:59 -0400

    removed jsx and imported + rendered components

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,51 +1,13 @@
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import Contact from "../components/Contact";
+import Intro from "../components/Intro";
+import PreviewProject from "../components/PreviewProject";
 
 function Index() {
   return (
-    <div className="grid">
-      <motion.div
-        className="card"
-        initial={{ x: "-200vw", rotateZ: 100 }}
-        animate={{ x: 0, rotateZ: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 35,
-        }}
-        whileHover={{ scale: 1.1, rotateZ: 10 }}
-      >
-        <Link to="/projects">Projects</Link>
-      </motion.div>
-      <motion.div
-        className="card"
-        initial={{ y: "-200vh", rotateZ: -50 }}
-        animate={{ y: 0, rotateZ: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 35,
-        }}
-        whileHover={{
-          scale: 1.25,
-          rotateZ: -15,
-        }}
-      >
-        <Link to="/about">About</Link>
-      </motion.div>
-      <motion.div
-        className="card"
-        initial={{ x: "200vw", rotateZ: 150 }}
-        animate={{ x: 0, rotateZ: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 35,
-        }}
-        whileHover={{
-          scale: 1.25,
-          rotateZ: 18,
-        }}
-      >
-        <Link to="/labs">Labs</Link>
-      </motion.div>
+    <div className="index">
+      <Intro />
+      <PreviewProject />
+      <Contact />
     </div>
   );
 }
