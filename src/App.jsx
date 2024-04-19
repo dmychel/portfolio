d255131ef5215a7cfc9ad270a9a2988ff288d9d4
dmychel <dmychel@tutanota.com>
2024-04-18 20:08:35 -0400

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Index from "./routes/Index";
 import Projects from "./routes/Projects";
 import About from "./routes/About";
 import Labs from "./routes/Labs";
+import YakuzaAPI from "./routes/projectPages/YakuzaAPI"
+import Terminal from "./routes/projectPages/Terminal"
+import Onitsuka from "./routes/projectPages/Onitsuka"
 import Error from "./routes/Error";
 
 import "/src/styles/app.css";
@@ -15,6 +18,9 @@ const App = () => {
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/yakuza-api" element={<YakuzaAPI />}></Route>
+          <Route path="/termial" element={<Terminal />}></Route>
+          <Route path="/onitsuka" element={<Onitsuka />}></Route>
           <Route path="/labs" element={<Labs />} />
           <Route path="*" element={<Error />} />
         </Routes>
