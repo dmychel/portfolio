548653197caac86fba5fce782fac161094b128ae
dmychel <dmychel@tutanota.com>
2024-04-18 21:28:37 -0400

    added animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,62 +1,154 @@
 import { motion } from "framer-motion"
 
 function Skills() {
-    return (
-        <motion.section className="skills" initial={{
+    const variants = {
+        hidden: { opacity: 0, },
+        show: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const stagger = {
+        hidden: {
             opacity: 0,
-            y: "200vh",
-        }}
+            y: 10
+        },
+        show: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 2,
+            },
+        }
+    }
+
+
+
+
+    return (
+        <motion.section className="skills"
+            variants={variants}
+            initial={{
+                opacity: 0,
+                y: "200vh",
+            }}
             animate={{
                 opacity: 1,
                 y: 0,
             }}
-            transition={{ delay: 0.25, type: "spring", stiffness: 50 }}>
+            transition={{ delay: 0.25, type: "spring", stiffness: 50 }} >
 
             <h2>Technologies I Like</h2>
             <div className="skill_tools">
-                <img
+                <motion.img
+                    initial={{
+                        y: 20,
+                        opacity: 0
+                    }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.25 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
                     alt="Javascript"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.35 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
                     alt="React"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.45 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
                     alt="Node"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.55 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg"
                     alt="Express"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.65 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg"
                     alt="MongoDB"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.75 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
                     alt="Sass"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.85 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"
                     alt="Linux"
                 />
 
-                <img
+                <motion.img initial={{
+                    y: 20,
+                    opacity: 0
+                }}
+                    animate={{
+                        y: 0,
+                        opacity: 1
+                    }}
+                    transition={{ delay: 1.95 }}
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg"
                     alt="Figma"
                 />
-
-
             </div>
-        </motion.section>
+        </ motion.section>
     )
 }
 
