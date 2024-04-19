2ab9a82a50792addb21c80c5b0d397334af3d816
dmychel <dmychel@tutanota.com>
2024-04-18 21:01:17 -0400

    render skills comp

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,12 +1,14 @@
 import Contact from "../components/Contact";
 import Intro from "../components/Intro";
 import PreviewProject from "../components/PreviewProject";
+import Skills from "../components/Skills";
 
 function Index() {
   return (
     <div className="index">
       <Intro />
       <PreviewProject />
+      <Skills />
       <Contact />
     </div>
   );
