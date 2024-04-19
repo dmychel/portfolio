d900ef52402a263d8bbefcda97a9a5939a8bcb87
dmychel <dmychel@tutanota.com>
2024-04-19 19:40:56 -0400

    removed routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import Index from "./routes/Index";
 import Projects from "./routes/Projects";
 import About from "./routes/About";
 import Labs from "./routes/Labs";
-import YakuzaAPI from "./routes/projectPages/YakuzaAPI"
-import Terminal from "./routes/projectPages/Terminal"
-import Onitsuka from "./routes/projectPages/Onitsuka"
+import ProjectPage from "./routes/projectPages/YakuzaAPI";
 import Error from "./routes/Error";
 
 import "/src/styles/app.css";
@@ -21,9 +19,7 @@ const App = () => {
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/yakuza-api" element={<YakuzaAPI />}></Route>
-          <Route path="/termial" element={<Terminal />}></Route>
-          <Route path="/onitsuka" element={<Onitsuka />}></Route>
+          <Route path="/project-page" element={<ProjectPage />}></Route>
           <Route path="/labs" element={<Labs />} />
           <Route path="*" element={<Error />} />
         </Routes>
