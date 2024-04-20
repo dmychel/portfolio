531bc6b3f740fd0d25f60732bc09d7036e19347a
dmychel <dmychel@tutanota.com>
2024-04-20 19:36:55 -0400

    imported and passed props

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,16 +2,21 @@ import Contact from "../components/Contact";
 import Intro from "../components/Intro";
 import PreviewProject from "../components/PreviewProject";
 import Skills from "../components/Skills";
+import PropTypes from "prop-types";
 
-function Index() {
+function Index({ setCurrentProject }) {
   return (
     <div className="index">
       <Intro />
-      <PreviewProject />
+      <PreviewProject setCurrentProject={setCurrentProject} />
       <Skills />
       <Contact />
     </div>
   );
 }
+
+Index.propTypes = {
+  setCurrentProject: PropTypes.func,
+};
 
 export default Index;
