3d13b3e3ba7b257f46474b2ff9931e717037b2e0
dmychel <dmychel@tutanota.com>
2024-04-20 19:36:32 -0400

    imported prop types and passed props to comps

--- src/components/PreviewProject.jsx
+++ src/components/PreviewProject.jsx
@@ -1,87 +1,98 @@
-import ProjectRender from "./ProjectRender"
-import { motion } from 'framer-motion'
-import { Link } from "react-router-dom"
+import ProjectRender from "./ProjectRender";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-
-function PreviewProject() {
-    const projects = [
+function PreviewProject({ setCurrentProject }) {
+  const projects = [
+    {
+      title: "Yakuza API",
+      description:
+        "A restful API I created to serve media assets from the Yakzua series",
+      live: "https://yakuza-api.netlify.app/",
+      image: "/assets/imgs/yakuza-api.png",
+      tools: [
         {
-            title: "Yakuza API",
-            description:
-                "A restful API I created to serve media assets from the Yakzua series",
-            live: "https://yakuza-api.netlify.app/",
-            image: "/assets/imgs/yakuza-api.png",
-            tools: [
-                {
-                    name: "Javascript",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-                },
-                {
-                    name: "node",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
-                },
-            ],
-            page: "/yakuza-api",
+          name: "Javascript",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
         },
         {
-            title: "Terminal",
-            description:
-                "A unique twist on my portfolio to show my passion for Linux and tech. *This project is in the process of being migrated, thank you for your patience.*",
-            live: "",
-            image: "/assets/imgs/yakuza-api.png",
-            tools: [
-                {
-                    name: "reactJS",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-                },
-                {
-                    name: "Vite",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
-                },
-                {
-                    name: "Sass",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
-                },
-            ],
-            page: "/terminal",
+          name: "node",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+        },
+      ],
+      page: "/yakuza-api",
+    },
+    {
+      title: "Terminal",
+      description:
+        "A unique twist on my portfolio to show my passion for Linux and tech. *This project is in the process of being migrated, thank you for your patience.*",
+      live: "",
+      image: "/assets/imgs/yakuza-api.png",
+      tools: [
+        {
+          name: "reactJS",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
         },
         {
-            title: "Onitsuka",
-            description: "A mock eCommerce website",
-            live: "https://onitsuka-tiger-mock.netlify.app/",
-            tools: [
-                {
-                    name: "reactJS",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-                },
-                {
-                    name: "Vite",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
-                },
-                {
-                    name: "Sass",
-                    url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
-                },
-            ],
-            page: "/onitsuka",
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
         },
-    ]
-    return (
-        <motion.section className="preview_project" initial={{
-            opacity: 0,
-            y: "200vh",
-        }}
-            animate={{
-                opacity: 1,
-                y: 0,
-            }}
-            transition={{ delay: 0.45, type: "spring", stiffness: 50 }}>
-            <h2>Projects</h2>
-            <ProjectRender projects={projects} />
-            <p style={{ color: "#50FA7B" }}>View more projects <Link to='/projects'>here</Link>.</p>
-
-        </motion.section>
-    )
+        {
+          name: "Sass",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+        },
+      ],
+      page: "/terminal",
+    },
+    {
+      title: "Onitsuka",
+      description: "A mock eCommerce website",
+      live: "https://onitsuka-tiger-mock.netlify.app/",
+      tools: [
+        {
+          name: "reactJS",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+        },
+        {
+          name: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+        },
+        {
+          name: "Sass",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
+        },
+      ],
+      page: "/onitsuka",
+    },
+  ];
+  return (
+    <motion.section
+      className="preview_project"
+      initial={{
+        opacity: 0,
+        y: "200vh",
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{ delay: 0.45, type: "spring", stiffness: 50 }}
+    >
+      <h2>Projects</h2>
+      <ProjectRender
+        projects={projects}
+        setCurrentProject={setCurrentProject}
+      />
+      <p style={{ color: "#50FA7B" }}>
+        View more projects <Link to="/projects">here</Link>.
+      </p>
+    </motion.section>
+  );
 }
 
-export default PreviewProject
+PreviewProject.propTypes = {
+  setCurrentProject: PropTypes.func,
+};
+
+export default PreviewProject;
