336fc98c0a1ac9de6aa23c55a226947fe92121e1
dmychel <dmychel@tutanota.com>
2024-04-21 19:09:46 -0400

    added summary property to the objects

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -3,6 +3,8 @@ export const projectArr = [
     title: "Yakuza API",
     description:
       "A restful API I created to serve media assets from the Yakzua series",
+    summary:
+      "When delving into a new technology or concept, the development community often emphasizes the importance of learning by creating projects that address real-world problems. However, brainstorming project ideas for technologies you have very little to no experince with can be challenging. What issues are there to solve when you're you've never used the technology? After several attempts with mundane CRUD apps, I recalled my search for a Yakuza API to utilize its resources, which led to the inception of this project. Although still in its beta stage, I'm uncertain about the extent to which I intend to expand it. Nevertheless, I plan to create and maintain a roadmap to guide its development.",
     live: "https://yakuza-api.netlify.app/",
     image:
       "https://64.media.tumblr.com/46bd487b81e3b49b3b3f523c5efd8683/e7f01d6be5be4ec0-2a/s2048x3072/604e8f9cbfcff03ac091f7d07d5dcc797478240c.pnj",
@@ -21,7 +23,9 @@ export const projectArr = [
   {
     title: "Terminal",
     description:
-      "A unique twist on my portfolio to show my passion for Linux and tech. *This project is in the process of being migrated, thank you for your patience.*",
+      "A unique twist on my portfolio to show my passion for Linux and tech.",
+    summary:
+      "I created this Terminal styled portfolio because at the time I had difficult time designing a portfolio that looked interresting enough to look at. Thhe project turned out to be a good challenge and forced me to gain a better understanding of useState and props in React. *This project is in the process of being migrated, thank you for your patience.*",
     live: "",
     image:
       "https://64.media.tumblr.com/d352971e6dbf1425eb1b694ae0aee2aa/0d06ed392c6e1280-08/s2048x3072/a1d176d5521aa3ea01288d85de5fa0c205b02635.jpg",
@@ -44,6 +48,8 @@ export const projectArr = [
   {
     title: "Onitsuka",
     description: "A mock eCommerce website",
+    summary:
+      "A mock eCommerce website that emulates the full customer experince.",
     live: "https://onitsuka-tiger-mock.netlify.app/",
     image:
       "https://64.media.tumblr.com/8dcf7ffed9c4b7d37d38488d91d69d00/355392f59f6cbd85-17/s2048x3072/104f08ed8a7328fba8302215c2f6e76cae4bce25.pnj",
@@ -65,7 +71,8 @@ export const projectArr = [
   },
   {
     title: "Message Board",
-    description:
+    description: "A dynamic NodeJS messaging app.",
+    summary:
       "A basic dynamic node application that displays messages from online users. This was a exciting project to begin my back-end journey. *The app needs to build when launched, please await around 45sec*",
     live: "https://message-board-apba.onrender.com/",
     repo: "https://github.com/dmychel/message-board",
@@ -88,7 +95,9 @@ export const projectArr = [
   },
   {
     title: "Windfall Matchup",
-    description: "Traditional Match up game to display my competency React",
+    description: "Traditional match up game.",
+    summary:
+      "A traditional match up card game that displays my competency using Fetch API, React useState and props.",
     live: "https://windfall-matchup.netlify.app/",
     repo: "https://github.com/dmychel/memory-game",
     image:
