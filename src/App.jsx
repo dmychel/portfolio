3d3cdb37ec85e7164cddded6d1d7d881c0dddc03
dmychel <dmychel@tutanota.com>
2024-04-21 22:07:01 -0400

    jsx edit and added useEffect function to set useState based on locals storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Index from "./routes/Index";
 import Projects from "./routes/Projects";
 import About from "./routes/About";
 import Labs from "./routes/Labs";
 import ProjectPage from "./routes/ProjectPage";
 import Error from "./routes/Error";
-
 import "/src/styles/app.css";
-import { useState } from "react";
+
 
 const App = () => {
   const [currentProject, setCurrentProject] = useState(null);
 
+  useEffect(() => {
+    const project = JSON.parse(localStorage.getItem('project'));
+    if (project) {
+      setCurrentProject(project);
+    }
+    console.log('hello')
+  }, []);
+
   return (
     <section className="app">
-      <header>
-        <h1>Dylon Crowley-Perez</h1>
-      </header>
       <BrowserRouter>
+        <header>
+          <Link to="/" style={{ color: "#f8f8f2", textDecoration: "underline" }}><h1>Dylon Crowley-Perez</h1></Link>
+        </header>
         <Routes>
           <Route
             path="/"
