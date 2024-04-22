712e22a7ecc308aded789e346a7ec29a6df14e28
dmychel <dmychel@tutanota.com>
2024-04-21 22:08:01 -0400

    addded conditional the renders either loading or the project page

--- src/routes/ProjectPage.jsx
+++ src/routes/ProjectPage.jsx
@@ -1,102 +1,116 @@
+// functions
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 function ProjectPage({ currentProject }) {
-  const object = currentProject;
+  const [loading, setLoading] = useState(true)
+  const object = currentProject
+
+  // wait for app to set useState
+  useEffect(() => {
+    currentProject === null ? setLoading(true) : setLoading(false)
+  }, [currentProject])
 
   return (
     <section className="project_page">
-      <motion.h3
-        initial={{
-          opacity: 0,
-          x: "-200vw",
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{ delay: 0.85, type: "spring", stiffness: 45 }}
-      >
-        {object.title}
-      </motion.h3>
-      <motion.span
-        initial={{
-          opacity: 0,
-          x: "-200vw",
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{ delay: 0.55, type: "spring", stiffness: 45 }}
-      >
-        <Link to="/projects">Back</Link>
-      </motion.span>
-      <motion.div
-        className="project_info"
-        initial={{
-          opacity: 0,
-          x: "-200vw",
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{ delay: 0.24, type: "spring", stiffness: 45 }}
-      >
-        <motion.div
-          className="project_image"
-          initial={{
-            opacity: 0,
-            x: "-200vw",
-          }}
-          animate={{
-            opacity: 1,
-            x: 0,
-          }}
-          transition={{ delay: 0.25, type: "spring", stiffness: 45 }}
-        >
-          <img src={object.image} alt="Error Loading Image :(" />
-        </motion.div>
-        <p>{object.summary}</p>
-      </motion.div>
-      <motion.div
-        className="project_links"
-        initial={{
-          opacity: 0,
-          x: "-200vw",
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{ delay: 0.25, type: "spring", stiffness: 45 }}
-      >
-        <a href={object.live} target="_blank" rel="noreferrer">
-          Live
-        </a>
-        <a href={object.repo} target="_blank" rel="noreferrer">
-          Repo
-        </a>
-      </motion.div>
+      {
+        loading ? <h2>Loading.. :D</h2> :
+          <>
+            <motion.h3
+              initial={{
+                opacity: 0,
+                x: "-200vw",
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ delay: 0.85, type: "spring", stiffness: 45 }}
+            >
+              {object.title}
+            </motion.h3>
+            <motion.span
+              initial={{
+                opacity: 0,
+                x: "-200vw",
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ delay: 0.55, type: "spring", stiffness: 45 }}
+            >
+              <Link to="/projects">.../projects</Link>
+            </motion.span>
+            <motion.div
+              className="project_info"
+              initial={{
+                opacity: 0,
+                x: "-200vw",
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ delay: 0.24, type: "spring", stiffness: 45 }}
+            >
+              <motion.div
+                className="project_image"
+                initial={{
+                  opacity: 0,
+                  x: "-200vw",
+                }}
+                animate={{
+                  opacity: 1,
+                  x: 0,
+                }}
+                transition={{ delay: 0.25, type: "spring", stiffness: 45 }}
+              >
+                <img src={object.image} alt="Error Loading Image :(" />
+              </motion.div>
+              <p>{object.summary}</p>
+            </motion.div>
+            <motion.div
+              className="project_links"
+              initial={{
+                opacity: 0,
+                x: "-200vw",
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ delay: 0.25, type: "spring", stiffness: 45 }}
+            >
+              <a href={object.live} target="_blank" rel="noreferrer">
+                Live
+              </a>
+              <a href={object.repo} target="_blank" rel="noreferrer">
+                Repo
+              </a>
+            </motion.div>
 
-      <motion.div
-        className="skill_tools"
-        initial={{
-          opacity: 0,
-          x: "-200vw",
-        }}
-        animate={{
-          opacity: 1,
-          x: 0,
-        }}
-        transition={{ type: "spring", stiffness: 45 }}
-      >
-        {object.tools.map((tool, index) => (
-          <motion.img src={tool.url} alt={tool.name} key={index} />
-        ))}
-      </motion.div>
+            <motion.div
+              className="skill_tools"
+              initial={{
+                opacity: 0,
+                x: "-200vw",
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ type: "spring", stiffness: 45 }}
+            >
+              {object.tools.map((tool, index) => (
+                <motion.img src={tool.url} alt={tool.name} key={index} />
+              ))}
+            </motion.div>
+          </>
+      }
+
     </section>
   );
 }
