776ade75436a36e0baaf43546dff7f6e1a465bb5
dmychel <dmychel@tutanota.com>
2024-04-21 22:08:29 -0400

    property edit

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -6,9 +6,9 @@ export const projectArr = [
     summary:
       "When delving into a new technology or concept, the development community often emphasizes the importance of learning by creating projects that address real-world problems. However, brainstorming project ideas for technologies, you have very little to no experince with can be challenging. What issues are there to solve when you're you've never used the technology? After several attempts with mundane CRUD apps, I recalled my search for a Yakuza API to utilize its resources, which led to the inception of this project. Although still in its beta stage, I'm uncertain about the extent to which I intend to expand it. Nevertheless, I plan to add and maintain this project.",
     live: "https://yakuza-api.netlify.app/",
+    repo: "https://github.com/dmychel/yakuza-api",
     image:
       "https://64.media.tumblr.com/46bd487b81e3b49b3b3f523c5efd8683/e7f01d6be5be4ec0-2a/s2048x3072/604e8f9cbfcff03ac091f7d07d5dcc797478240c.pnj",
-    repo: "https://github.com/dmychel/yakuza-api",
     tools: [
       {
         name: "Javascript",
@@ -26,10 +26,10 @@ export const projectArr = [
       "A unique twist on my portfolio to show my passion for Linux and tech.",
     summary:
       "I created this Terminal styled portfolio because at the time I had difficult time designing a portfolio that looked interresting enough to look at. Thhe project turned out to be a good challenge and forced me to gain a better understanding of useState and props in React. *This project is in the process of being migrated, thank you for your patience.*",
-    live: "",
+    live: null,
+    repo: null,
     image:
       "https://64.media.tumblr.com/d352971e6dbf1425eb1b694ae0aee2aa/0d06ed392c6e1280-08/s2048x3072/a1d176d5521aa3ea01288d85de5fa0c205b02635.jpg",
-    repo: "",
     tools: [
       {
         name: "reactJS",
@@ -73,7 +73,7 @@ export const projectArr = [
     title: "Message Board",
     description: "A dynamic NodeJS messaging app.",
     summary:
-      "A basic dynamic node application that displays messages from online users. This was a exciting project to begin my back-end journey. *The app needs to build when launched, please await around 45sec*",
+      "A basic dynamic node application that displays messages from online users. This was a exciting project to begin my back-end journey. *The app needs to build when launched, please wait around 45sec*",
     live: "https://message-board-apba.onrender.com/",
     repo: "https://github.com/dmychel/message-board",
     image:
@@ -97,7 +97,7 @@ export const projectArr = [
     title: "Windfall Matchup",
     description: "Traditional match up game.",
     summary:
-      "A traditional match up card game that displays my competency using Fetch API, React useState and props.",
+      "A traditional match up card game that displays my competency using Fetch API, React useState, and props.",
     live: "https://windfall-matchup.netlify.app/",
     repo: "https://github.com/dmychel/memory-game",
     image:
